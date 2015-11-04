4a202086dbb71f49c4cc831aada531c07a7f0595
samyoshon <samuelyshon@gmail.com>
2015-11-03 19:25:50 -0800

    revert all changes

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -2,8 +2,8 @@ class BusinessesController < ApplicationController
 
 	def index
 		@businesses = Business.search(params[:search])
-		# @featured_business = Business.where("rating >= ?", 4).all.sample ## AND WHERE RESTAURANT IS NEAR A CERTAIN ZIPCODE BASED OFF CHROME LOCATION
-		# @featured_menu = Menu.where(business_id: @featured_business.id).all.sample
+		@featured_business = Business.where("rating >= ?", 4).all.sample ## AND WHERE RESTAURANT IS NEAR A CERTAIN ZIPCODE BASED OFF CHROME LOCATION
+		@featured_menu = Menu.where(business_id: @featured_business.id).all.sample
 	end
 
 	def new
